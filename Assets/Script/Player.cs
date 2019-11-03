bd541de9ce400ffe75e5c7f08851ac2ae1c05baf
beohoang98 <beohoang98@users.noreply.github.com>
2019-11-03 12:27:35 +0700

    add texture and some level01 map part

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    SpriteRenderer renderer;
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid = GetComponent<Rigidbody2D>();
+        this.renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.followMouse();
+    }
+
+    private void followMouse()
+    {
+        Vector2 mousePosition = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = new Vector2(
+                mousePosition.x - transform.position.x,
+                mousePosition.y - transform.position.y
+            );
+        direction.Normalize();
+
+        if (direction.x < 0)
+        {
+            this.renderer.flipX = true;
+        } else
+        {
+            this.renderer.flipX = false;
+        }
+        
+        this.rigid.velocity = direction.normalized * this.speed;
+    }
+}
